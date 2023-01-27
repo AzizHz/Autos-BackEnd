48e74ffb6d7825403ff7bf49f9553c001da2c413
Aziz-Hz <youssef.abidup@gmail.com>
2023-01-27 15:38:04 +0100

    Fixing bugs

--- server/Routes/ChatRouter.js
+++ server/Routes/ChatRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const controller = require('../controllers/ChatController');
+const controller = require('../Controllers/ChatController');
 const router = express.Router()
 
 router.post('/', controller.createChat);
