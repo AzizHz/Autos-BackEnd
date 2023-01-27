0226f2bec384a69a04b4ad74c07e1c6bd324e86a
Aziz-Hz <youssef.abidup@gmail.com>
2023-01-27 15:18:46 +0100

    deplyment configuration

--- server/Database/Connection.js
+++ server/Database/Connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         //mongodb connection string
-        const con = await mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        const con = await mongoose.connect(process.env.MONGO_URL,)
         console.log(`MongoDB connected :${con.connection.host}`);
     } catch (err) {
         console.log(err);
