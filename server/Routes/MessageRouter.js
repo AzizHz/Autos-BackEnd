9cf19a65c08890db58601134da33df35fafe39f1
Aziz-Hz <hzami.ahmedaziz@gmail.com>
2023-05-01 11:56:01 +0100

    sse implimentation

--- server/Routes/MessageRouter.js
+++ server/Routes/MessageRouter.js
@@ -6,5 +6,7 @@ const route = express.Router();
 route.post('/', controller.addMessage);
 
 route.get('/:chatId', controller.getMessages);
+route.get('/close/:chatId', controller.closeConnection);
+
 
 module.exports = route 
